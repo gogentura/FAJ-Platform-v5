bbf0e27d53a2046aa4a0ecb00dcec0393fdbe8ec
gogentura <gogentura@gmail.com>
2026-07-22 10:59:49 +0300

    Delete app/rating_cache.py

--- app/rating_cache.py
+++
@@ -1,8 +0,0 @@
-# Простой кеш для FAJ Rating
-rating_cache = {}
-
-def update_rating(team: str, rating: float):
-    rating_cache[team] = rating
-
-def get_rating(team: str, default: float = 100) -> float:
-    return rating_cache.get(team, default)
