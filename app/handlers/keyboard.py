576bd96dbb531e8c26495b23dbebc017c57026f4
gogentura <gogentura@gmail.com>
2026-07-23 11:46:44 +0300

    Update keyboard.py

--- app/handlers/keyboard.py
+++ app/handlers/keyboard.py
@@ -1,8 +1,33 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 def get_main_keyboard():
+
     buttons = [
-        [KeyboardButton(text="📊 Статус"), KeyboardButton(text="📋 Журнал")],
-        [KeyboardButton(text="❓ Помощь")]
+
+        [
+            KeyboardButton(text="📊 Статус"),
+            KeyboardButton(text="⚽ Прогноз")
+        ],
+
+        [
+            KeyboardButton(text="📚 Паспорт"),
+            KeyboardButton(text="👥 Команды")
+        ],
+
+        [
+            KeyboardButton(text="🏆 Таблица"),
+            KeyboardButton(text="🌍 Лиги")
+        ],
+
+        [
+            KeyboardButton(text="📋 Журнал"),
+            KeyboardButton(text="❤️ Проверка")
+        ]
+
     ]
-    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+    return ReplyKeyboardMarkup(
+        keyboard=buttons,
+        resize_keyboard=True
+    )
