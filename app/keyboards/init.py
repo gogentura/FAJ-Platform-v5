8a97bd592f0ad9ce32c4726593b985b71c47cadc
gogentura <gogentura@gmail.com>
2026-07-24 08:21:27 +0300

    Update init.py

--- app/keyboards/init.py
+++ app/keyboards/init.py
@@ -1,2 +1,11 @@
+# =====================================================
+# FAJ Platform v6.0
+# Keyboards Package
+# =====================================================
+
+
 from .main import main_keyboard
+
 from .admin import admin_keyboard
+
+from .fixtures import fixtures_keyboard
