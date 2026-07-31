e21147c3450b58b881b8ff32b9209ccfe7e2ef36
gogentura <gogentura@gmail.com>
2026-07-31 21:24:57 +0300

    Create __init__.py

---
+++ app/api/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+FAJ Platform v10.0 - API Clients
+"""
+
+from app.api.football_api import FootballAPI
+from app.api.football_data_api import FootballDataAPI
+
+__all__ = ["FootballAPI", "FootballDataAPI"]
