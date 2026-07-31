1bc72199a59d916abc4df814fcc70789159911d5
gogentura <gogentura@gmail.com>
2026-07-31 22:20:50 +0300

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-FAJ Platform v10.0 - API Clients
+FAJ Platform v10.0 - API Package
 """
 
 from app.api.football_api import FootballAPI
-from app.api.football_data_api import FootballDataAPI
+from app.api.football_data import FootballDataAPI
+from app.api.ids import IDs
 
-__all__ = ["FootballAPI", "FootballDataAPI"]
+__all__ = ["FootballAPI", "FootballDataAPI", "IDs"]
