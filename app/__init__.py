fe771fe65b1a51da24669b7bde565509a179677d
gogentura <gogentura@gmail.com>
2026-08-01 21:31:34 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,7 @@
 FAJ Platform v10.0 - App Package
 """
 
-# Импорты вынесены напрямую, чтобы избежать циклических зависимостей
-# from app.database import FAJDatabase
-# from app.faj_match_engine import FAJMatchEngine
+# Пустой __init__.py чтобы избежать циклических импортов
+# Все импорты делаются напрямую из файлов
 
 __all__ = []
