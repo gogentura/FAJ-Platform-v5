a23ac72a60689972fec0239a6dc3f9f6f66b7602
gogentura <gogentura@gmail.com>
2026-08-01 21:20:42 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,8 @@
 FAJ Platform v10.0 - App Package
 """
 
-from app.api.football_api import FootballAPI
-from app.api.football_data import FootballDataAPI
-from app.api.ids import IDs
-from app.database import FAJDatabase
-from app.faj_match_engine import FAJMatchEngine
+# Импорты вынесены напрямую, чтобы избежать циклических зависимостей
+# from app.database import FAJDatabase
+# from app.faj_match_engine import FAJMatchEngine
 
-__all__ = ["FootballAPI", "FootballDataAPI", "IDs", "FAJDatabase", "FAJMatchEngine"]
+__all__ = []
