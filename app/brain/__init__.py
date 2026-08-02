e20cf0e3220f379574e605374dcd62082ab12bfd
gogentura <gogentura@gmail.com>
2026-08-02 21:42:34 +0300

    Create  __init__.py

---
+++ app/brain/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+FAJ Platform v10.1
+Brain Package
+
+Центр обучения, памяти и самоанализа модели FAJ
+"""
+
+from .data_brain import FAJDataBrain
+from .learning_brain import FAJLearningBrain
+
+
+__all__ = [
+    "FAJDataBrain",
+    "FAJLearningBrain"
+]
