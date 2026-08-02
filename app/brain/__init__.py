80fde0f16879b105561dc8da2a223dced27b6376
gogentura <gogentura@gmail.com>
2026-08-02 23:13:39 +0300

    Update __init__.py

--- app/brain/__init__.py
+++ app/brain/__init__.py
@@ -3,13 +3,38 @@
 
 """
 FAJ Platform v10.0
-Brain Module
+Brain System
 
-Мозг FAJ Platform
+Главный загрузчик мозга FAJ
 """
-
-# Пока импортируем только готовые модули
 
 from .memory_brain import FAJMemoryBrain
 from .learning_brain import FAJLearningBrain
 from .correction_brain import FAJCorrectionBrain
+
+
+__all__ = [
+    "FAJMemoryBrain",
+    "FAJLearningBrain",
+    "FAJCorrectionBrain"
+]
+
+
+def get_brain_status():
+
+    memory = FAJMemoryBrain()
+    learning = FAJLearningBrain()
+    correction = FAJCorrectionBrain()
+
+    return {
+
+        "memory":
+            memory.get_statistics(),
+
+        "learning":
+            learning.get_status(),
+
+        "correction":
+            correction.get_status()
+
+    }
