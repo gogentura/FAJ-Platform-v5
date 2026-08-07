f3f806b56d0b2e07a6c62709a95d5924c438c3e0
gogentura <gogentura@gmail.com>
2026-08-07 07:53:43 +0300

    Create __init__.py

---
+++ app/replay/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+FAJ Replay — модуль исторической реконструкции
+"""
+
+from .historical_replay import HistoricalReplay
+
+__all__ = [
+    'HistoricalReplay',
+]
