e9b3a693517ebfba03c5a0fd6b865f728a9e4ccc
gogentura <gogentura@gmail.com>
2026-08-11 23:41:48 +0300

    Create rpl_sources.py

---
+++ app/parsers/rpl_sources.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+FAJ Platform v12.1
+RPL Parser Sources
+"""
+
+RPL_SOURCES = {
+
+    "smart_tables": {
+        "name": "Smart Tables",
+        "url": "https://smart-tables.ru/league/Russia/Premier_League",
+        "type": "league",
+    },
+
+    "championat": {
+        "name": "Чемпионат",
+        "url": "https://www.championat.com/football/_russiapl/tournament/6594/calendar/",
+        "type": "calendar",
+    },
+
+    "soccerland": {
+        "name": "Soccerland",
+        "url": "https://soccerland.ru/russia/premier-liga/2026-2027",
+        "type": "league",
+    },
+}
+
+
+SEASON = "2026-2027"
+LEAGUE = "РПЛ"
+COMPETITION = "РПЛ"
