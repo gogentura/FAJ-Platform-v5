2aaa435389b9d4882588f31ff50a5386ba2a09ff
gogentura <gogentura@gmail.com>
2026-08-20 10:49:52 +0300

    Create __init__.py

---
+++ app/etc/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+FAJ Platform v12.1
+Evolution Training Center (ETC)
+
+ETC package.
+
+Назначение:
+    Пакет Evolution Training Center.
+
+Основные модули:
+    observed_xg.py
+    xg_calibration.py
+    statistical_analyzer.py
+    prediction_error_analyzer.py
+    club_rating_updater.py
+    learning_memory.py
+    batch_controller.py
+    evolution_training_center.py
+
+Принцип:
+    __init__.py не выполняет обучение,
+    не изменяет БД и не запускает ETC автоматически.
+"""
+
+__version__ = "1.0"
+__name__ = "FAJ ETC"
